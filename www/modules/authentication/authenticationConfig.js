0d2d2b9f8a9c06e9fc1187660fa42b09e8bbfe6a
yann <yann.boisselier@gmail.com>
2015-02-22 00:48:11 +0100

    add i18n support for authentication module

--- www/modules/authentication/authenticationConfig.js
+++ www/modules/authentication/authenticationConfig.js
@@ -10,20 +10,27 @@ angular.module('app.authentication')
         url: '/signin',
         views: {
           'me': {
-            templateUrl: 'modules/authentication/templates/signInForm.html',
-            controller: 'signInCtrl'
+            templateUrl: 'modules/authentication/templates/signInForm.html'
           }
         }
       });
 
     $translateProvider.translations('en', {
-      AUTHENTICATION_SIGNOUT: "Sign out",
-      AUTHENTICATION_SIGNIN: "Sign in"
+      AUTHENTICATION_SIGNOUT_BUTTON: "Sign out",
+      AUTHENTICATION_SIGNIN_BUTTON: "Sign in",
+      AUTHENTICATION_SIGNIN_FORM_EMAIL_LABEL: "Email",
+      AUTHENTICATION_SIGNIN_FORM_EMAIL_ERROR_REQUIRED:"Email is required",
+      AUTHENTICATION_SIGNIN_FORM_PASSWORD_LABEL:"Password",
+      AUTHENTICATION_SIGNIN_FORM_PASSWORD_ERROR_REQUIRED:"Password is required"
     });
 
     $translateProvider.translations('fr', {
-      AUTHENTICATION_SIGNOUT: "Se déconnecter",
-      AUTHENTICATION_SIGNIN: "Se connecter"
+      AUTHENTICATION_SIGNOUT_BUTTON: "Se déconnecter",
+      AUTHENTICATION_SIGNIN_BUTTON: "Se connecter",
+      AUTHENTICATION_SIGNIN_FORM_EMAIL_LABEL:"Email",
+      AUTHENTICATION_SIGNIN_FORM_EMAIL_ERROR_REQUIRED:"L'email est requis",
+      AUTHENTICATION_SIGNIN_FORM_PASSWORD_LABEL:"Mot de passe",
+      AUTHENTICATION_SIGNIN_FORM_PASSWORD_ERROR_REQUIRED:"Le mot de passe est requis"
     });
 
   }]);
