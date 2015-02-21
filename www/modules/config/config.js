13784316c848a7f3e750a804bf70e1d29ceae9d9
yann <yann.boisselier@gmail.com>
2015-02-21 21:20:40 +0100

    remove upload zip file

--- www/modules/config/config.js
+++ www/modules/config/config.js
@@ -3,6 +3,7 @@
  * Inject "config" in controllers / services etc ... to use.
  */
 angular.module('app.config', [])
+
   .constant('config', {
     // base url for webservices, point to nodejs server
     serverUrl: 'http://91.121.166.167:3333'
